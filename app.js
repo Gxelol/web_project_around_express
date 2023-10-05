d5733c3f0c6090512b547ff6eaefe13b5a35d140
Gabriel Gonçalves <gabrielqgoncalves26@gmail.com>
2023-10-04 22:28:09 -0300

    Update: updating router calls

--- app.js
+++ app.js
@@ -6,7 +6,11 @@ const app = express();
 const userRouter = require('./routes/users');
 const cardRouter = require('./routes/cards');
 
-app.use('/', userRouter, cardRouter);
+app.use(userRouter);
+app.use(cardRouter);
+app.use('/', (req, res) => {
+  res.status(404).send({ message: 'A solicitação não foi encontrada' });
+});
 
 app.listen(PORT, () => {
   console.log(`O App está escutando na porta ${PORT}`);
