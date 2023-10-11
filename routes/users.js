6162ea967ace7b40b25b0ad145d461fa28791894
Gabriel Gonçalves <gabrielqgoncalves26@gmail.com>
2023-10-10 23:03:10 -0300

    Update: update users routes

--- routes/users.js
+++ routes/users.js
@@ -1,33 +1,12 @@
 const router = require('express').Router();
-const path = require('path');
-const fsPromises = require('fs').promises;
+const {
+  getUsers, getUserById, createUser, updateUser, updateUserAvatar,
+} = require('../controllers/users');
 
-const userPath = path.join(__dirname, '../data/users.json');
-
-router.get('/users', (req, res) => {
-  fsPromises.readFile(userPath, { encoding: 'utf8' })
-    .then((users) => {
-      res.send({ data: JSON.parse(users) });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-router.get('/users/:id', (req, res) => {
-  fsPromises.readFile(userPath, { encoding: 'utf8' })
-    .then((users) => {
-      const parsedUsersData = JSON.parse(users);
-      const user = parsedUsersData.find((user) => user._id === req.params.id);
-      if (!user) {
-        res.status(404).send({ message: 'Usuário não encontrado' });
-      } else {
-        res.send({ data: user });
-      }
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
+router.get('/', getUsers);
+router.get('/:id', getUserById);
+router.post('/', createUser);
+router.patch('/:id', updateUser);
+router.patch('/:id', updateUserAvatar);
 
 module.exports = router;
