c0a1efd5d070ad5cfb0f520e767fd3b33044ffd5
Gabriel Gonçalves <gabrielqgoncalves26@gmail.com>
2023-10-10 23:03:20 -0300

    Update: update cards routes

--- routes/cards.js
+++ routes/cards.js
@@ -1,17 +1,12 @@
 const router = require('express').Router();
-const path = require('path');
-const fsPromises = require('fs').promises;
+const {
+  getCards, createCard, deleteCardById, likeCard, dislikeCard,
+} = require('../controllers/cards');
 
-const cardsPath = path.join(__dirname, '../data/cards.json');
-
-router.get('/cards', (req, res) => {
-  fsPromises.readFile(cardsPath, { encoding: 'utf8' })
-    .then((cards) => {
-      res.send({ data: JSON.parse(cards) });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
+router.get('/', getCards);
+router.delete('/:cardId', deleteCardById);
+router.post('/', createCard);
+router.put('/:cardId', likeCard);
+router.delete('/:cardId', dislikeCard);
 
 module.exports = router;
