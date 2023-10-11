8de0e28be3e049b353f98735316c9d6663e33304
Gabriel Gonçalves <gabrielqgoncalves26@gmail.com>
2023-10-10 23:02:08 -0300

    Update: update app

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const { PORT = 3000 } = process.env;
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect('mongodb://localhost:27017/aroundb', {
+  useNewUrlParser: true,
+});
+
+app.use((req, res, next) => {
+  req.user = {
+    _id: '6525d2283c0ae290f590eebc',
+  };
+
+  next();
+});
+
 const userRouter = require('./routes/users');
 const cardRouter = require('./routes/cards');
 
-app.use(userRouter);
-app.use(cardRouter);
+app.use('/users', userRouter);
+app.use('/cards', cardRouter);
+
 app.use('/', (req, res) => {
   res.status(404).send({ message: 'A solicitação não foi encontrada' });
 });
